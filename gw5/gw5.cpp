de61a234d8c6b8e6bf8a5f8282e7588d7476d237
Dustmaner <erick.soy@hotmail.com>
2018-07-23 21:12:21 -0500

    Variation in clicks position and delay to avoid detection

--- gw5/gw5.cpp
+++ gw5/gw5.cpp
@@ -8,6 +8,9 @@
 #include <conio.h>
 #include <fstream>
 #include <string>
+#include <stdlib.h> 
+#include <time.h>       
+
 using namespace std;
 
 
@@ -17,8 +20,23 @@ void main()
 	ifstream inFile;
 	int counter = 0;
 	int x;
+	int offset = 0;
 	string go = "y";
 	string fileChoose = "1.txt";
+	/* initialize random seed: */
+	srand(time(NULL));
+	int variationX = rand() % 10 -5;
+	int variationY = rand() % 10 -5;
+	int variantonD = rand() % 20 - 10;
+
+	inFile.open("Settings.txt");
+	if (!inFile) {
+		cerr << "Unable to open file " << "Settings.txt" << endl;
+		exit(1);   // call system to stop
+	}
+
+	inFile >> offset;
+	inFile >> sleepTime;
 
 	while (go != "`" || '\0') {
 		cout << "Loop times: " << counter++ << endl;
@@ -26,104 +44,107 @@ void main()
 		getline(cin, go);
 		fileChoose = go + ".txt";
 
+		inFile.close();
 		inFile.open(fileChoose);
 		if (!inFile) {
-			cerr << "Unable to open file datafile.txt";
+			cerr << "Unable to open file "<< fileChoose << endl;
 			exit(1);   // call system to stop
 		}
-
-		inFile >> sleepTime;
+		
 
 		while (inFile >> x) {
 			//MC
 			if (x == 11) {
-				SetCursorPos(-462, 714);
+				SetCursorPos(0 + offset + variationX, 714 + variationY);
 			}//1
 			if (x == 12) {
-				SetCursorPos(-426, 716);
+				SetCursorPos(36 + offset + variationX, 716 + variationY);
 			}//2
 			if (x == 13) {
-				SetCursorPos(-462, 752);
+				SetCursorPos(0 + offset + variationX, 752 + variationY);
 			}//3
 			if (x == 14) {
-				SetCursorPos(-426, 752);
+				SetCursorPos(36 + offset + variationX, 752 + variationY);
 			}//4
 
 			//2nd Char
 			if (x == 21) {
-				SetCursorPos(-381, 716);
+				SetCursorPos(81 + offset + variationX, 716 + variationY);
 			}//1
 			if (x == 22) {
-				SetCursorPos(-344, 716);
+				SetCursorPos(118 + offset + variationX, 716 + variationY);
 			}//2
 			if (x == 23) {
-				SetCursorPos(-381, 752);
+				SetCursorPos(81 + offset + variationX, 752 + variationY);
 			}//3
 			if (x == 24) {
-				SetCursorPos(-344, 752);
+				SetCursorPos(118 + offset + variationX, 752 + variationY);
 			}//4
 
 			//3rd Char
 			if (x == 31) {
-				SetCursorPos(-299, 716);
+				SetCursorPos(163 + offset + variationX, 716 + variationY);
 			}//1
 			if (x == 32) {
-				SetCursorPos(-263, 716);
+				SetCursorPos(199 + offset + variationX, 716 + variationY);
 			}//2
 			if (x == 33) {
-				SetCursorPos(-299, 752);
+				SetCursorPos(163 + offset + variationX, 752 + variationY);
 			}//3
 			if (x == 34) {
-				SetCursorPos(-263, 752);
+				SetCursorPos(199 + offset + variationX, 752 + variationY);
 			}//4
 
 			//4th Char
 			if (x == 41) {
-				SetCursorPos(-219, 716);
+				SetCursorPos(243 + offset + variationX, 716 + variationY);
 			}//1
 			if (x == 42) {
-				SetCursorPos(-183, 716);
+				SetCursorPos(279 + offset + variationX, 716 + variationY);
 			}//2
 			if (x == 43) {
-				SetCursorPos(-219, 752);
+				SetCursorPos(243 + offset + variationX, 752 + variationY);
 			}//3
 			if (x == 44) {
-				SetCursorPos(-183, 752);
+				SetCursorPos(279 + offset + variationX, 752 + variationY);
 			}//4
 
 			//Skills for 1 Char
 			if (x == 1) {
-				SetCursorPos(-352, 399);
+				SetCursorPos(110 + offset + variationX, 399 + variationY);
 				Sleep(100);
 			}//1
 			if (x == 2) {
-				SetCursorPos(-260, 399);
+				SetCursorPos(202 + offset + variationX, 399 + variationY);
 				Sleep(100);
 			}//2
 			if (x == 3) {
-				SetCursorPos(-168, 399);
+				SetCursorPos(294 + offset + variationX, 399 + variationY);
 				Sleep(100);
 			}//3
 			if (x == 4) {
-				SetCursorPos(-353, 559);
+				SetCursorPos(110 + offset + variationX, 559 + variationY);
 				Sleep(100);
 			}//4
 			if (x == 5) {
-				SetCursorPos(-260, 559);
+				SetCursorPos(202 + offset + variationX, 559 + variationY);
 				Sleep(100);
 			}//5
 			if (x == 6) {
-				SetCursorPos(-168, 559);
+				SetCursorPos(294 + offset + variationX, 559 + variationY);
 				Sleep(100);
 			}//6
 
 			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-			Sleep(sleepTime);
+			Sleep(sleepTime + variantonD);
+			cout << "(" << variationX << ", " << variationY << ")\n";
+			variationX = rand() % 12 -6;
+			variationY = rand() % 12 -6;
 		}
-		SetCursorPos(-126, 473);//Attack
+		SetCursorPos(336 + offset + variationX, 473 + variationY);//Attack
 		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-		Sleep(sleepTime);
-		SetCursorPos(-440, 500);//AUTO
+		Sleep(sleepTime + variantonD);
+		SetCursorPos(22 + offset + variationX, 505 + variationY);//AUTO
 		mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 
 		inFile.close();
